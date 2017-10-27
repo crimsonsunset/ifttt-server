447835d5c861100266ae9252328efda06898b8f9
Joe Sangiorgio <jsangio1@gmail.com>
2017-10-27 00:52:17 -0400

    feat: clean up axios request. prototype working

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -2,91 +2,67 @@ const router = require("express").Router();
 const axios = require('axios');
 const {delay, lowerCase, map} = require('lodash');
 const {catchErrors} = require('../util/helpers');
-
-const baseURL = process.NGROK_URL;
-
-
-function _handleItem(req, res) {
-	const {commands} = req.body;
-	const requester = req.headers.origin;
-	const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-
-
-	function sendAndAwaitCall(e, i) {
-		const urlz = 'https://jsonplaceholder.typicode.com/posts/1';
-		const {
-			itemName,
-			deferNext,
-			action = '',
-			request: {
-				verb,
-				body
-			}
-		} = e;
-
-		// const d = _callAxios(baseURL + itemName + action, verb, body);
-
-		return new Promise((resolve, reject) => {
-
-			const time = 1500;
-			delay(() => {
-				const promise = axios['get'](urlz, body);
-				console.log('about to resolvezz', body);
-				return resolve(promise)
-			}, deferNext);
-		});
-	}
-
-	let wrapped = catchErrors(sendAndAwaitCall);
-
-	(async  () => {
-		for (let i = 0; i < commands.length; i++) {
-			await wrapped(commands[i]);
-		}
-		console.log('all done making requests');
-		return res.json({status: 'worked'})
-	})();
-
-}
-
-router.post('/item/', _handleItem);
-
-
-function breathe(amount) {
-	return new Promise((resolve, reject) => {
-		if (amount < 500) {
-			reject('That is too small of a value');
-		}
-		setTimeout(() => resolve(`Done for ${amount} ms`), amount);
-	});
-}
-
-
-async function go(name, last) {
-	console.log(`Starting for ${name} ${last}!`);
-	const res = await breathe(1000);
-	console.log(res);
-	const res2 = await breathe(300);
-	console.log(res2);
-	const res3 = await breathe(750);
-	console.log(res3);
-	const res4 = await breathe(900);
-	console.log(res4);
-	console.log('end');
-}
-
-//
-// const wrappedFunction = catchErrors(go);
-// wrappedFunction('Wes', 'Bos');
-// go('Wes', 'Bos');
-
+const baseURL = process.env.NGROK_URL;
 
 router.get('/test/', function (req, res) {
-	const {itemName, action} = req.body;
-	const requester = req.headers.origin;
-	const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-	console.log('asd', fullUrl, requester);
-	res.json({status: 'worked'})
+    const {itemName, action} = req.body;
+    const requester = req.headers.origin;
+    const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    console.log('asd', fullUrl, requester);
+    res.json({status: 'worked'})
 });
+
+
+router.post('/:name', (req, res)=>{
+    const axiosConfig = {
+        headers: {
+            'Content-Type': 'text/plain'
+        }
+    };
+
+    const {name} = req.params;
+    const {commands} = req.body;
+
+    // const requester = req.headers.origin;
+    // const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+
+    function sendAndAwaitCall(e, i) {
+        const urlz = 'https://postman-echo.com/delay/3';
+        const {
+            itemName,
+            deferNext,
+            action,
+            request: {
+                verb,
+                body
+            }
+        } = e;
+
+        // const d = _callAxios(baseURL + itemName + action, verb, body);
+
+        return new Promise((resolve, reject) => {
+
+            const time = 1500;
+            delay(() => {
+                const suffix = (action) ? `${itemName}_${action}` : itemName;
+                const reqURL = `${baseURL}${name}/${suffix}`;
+                const promise = axios[verb](reqURL, body, axiosConfig);
+                return resolve(promise)
+            }, deferNext);
+        });
+    }
+
+    let wrapped = catchErrors(sendAndAwaitCall);
+
+    (async () => {
+        for (let i = 0; i < commands.length; i++) {
+            await wrapped(commands[i]);
+        }
+        console.log('all done making requests');
+        return res.json({status: 'worked'})
+    })();
+
+});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,24 +3,24 @@ const express = require('express');
 
 const app = express();
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const {isValidRequest, validateRequest} = require('./util/helpers');
 const routes = require("./controllers/mainController.js");
 
-// listen for requests :)
-
+process.env.domains = JSON.parse(process.env.ALLOWED_DOMAINS);
 process.env.domains = JSON.parse(process.env.ALLOWED_DOMAINS);
 
 const port = process.env.PORT || 4000;
 app.set('port', (port));
 
 app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
+
 
 //set up router
 app.use('/api/v1', routes);
@@ -30,5 +30,5 @@ app.use(isValidRequest);
 
 
 app.listen(app.get('port'), function () {
-	console.log(`Example app listening on portz ${port}!`)
+    console.log(`Example app listening on portz ${port}!`)
 });
